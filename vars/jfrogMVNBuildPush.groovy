27340ff84c4b9b7e22760bf2e31ac05be5101a64
rahuldevopslearn <131506642+rahuldevopslearn@users.noreply.github.com>
2024-10-18 15:45:10 +0530

    Update jfrogMVNBuildPush.groovy

--- vars/jfrogMVNBuildPush.groovy
+++ vars/jfrogMVNBuildPush.groovy
@@ -1,8 +1,8 @@
 def call(){
     withCredentials([usernamePassword(
-            jcredentialsId: "JfrogP",
-            JFROG_USERNAME: "USER",
-            JFROG_PASSWORD: "PASS"
+            credentialsId: "JfrogP",
+           usernameVariable: "USER",
+            passwordVariable: "PASS"
     )])  {
          sh 'curl -X PUT -u '$USER':'$PASS' -T target/*.jar http://44.211.126.159:8082/artifactory/example-repo-local/myapp.jar'
     }
