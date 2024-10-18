38d17e05a0a6001f0ce3d30d3096d258de564ee8
rahuldevopslearn <131506642+rahuldevopslearn@users.noreply.github.com>
2024-10-18 15:34:24 +0530

    Update jfrogMVNBuildPush.groovy

--- vars/jfrogMVNBuildPush.groovy
+++ vars/jfrogMVNBuildPush.groovy
@@ -4,6 +4,6 @@ def call(){
             JFROG_USERNAME: "USER",
             JFROG_PASSWORD: "PASS"
     )])  {
-         sh 'curl -X PUT -u $USER:$PASS -T target/*.jar http://44.211.126.159:8082/artifactory/example-repo-local/myapp.jar'
+         sh 'curl -X PUT -u '$USER':'$PASS' -T target/*.jar http://44.211.126.159:8082/artifactory/example-repo-local/myapp.jar'
     }
 }
